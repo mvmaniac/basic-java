3b90b69e7d9e2de5d8f82dcc456425adca74bd18
yhs <mvmaniaz@gmail.com>
2020-01-10 00:27:01 +0900

    [수정] Objects: 1장 티켓 판매 애플리케이션 수정 1차 - Theater 클래스로부터 TicketSeller, Audience 클래스를 캡슐화

--- src/main/java/io/devfactory/objects/ticket/Theater.java
+++ src/main/java/io/devfactory/objects/ticket/Theater.java
@@ -6,28 +6,8 @@ public class Theater {
     private TicketSeller ticketSeller;
 
     public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            // 관람객이 가방에 초대장이 있는 경우
-            // 판매원이 매표소에서 티켓을 내줌
-            final Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-
-            // 관람객이 티켓을 가방에 넣음
-            audience.getBag().putTicket(ticket);
-
-        } else {
-            // 관람객이 가방에 초대장이 없는 경우
-            // 판매원이 매표소에서 티켓을 내줌
-            final Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-
-            // 관람객 가방에 티켓 가격만큼 현금이 차감
-            audience.getBag().minusAmount(ticket.getFee());
-
-            // 판매원의 매표소에 티켓 가격만큼 판매금액이 증가
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-
-            // 관람객이 티켓을 가방에 넣음
-            audience.getBag().putTicket(ticket);
-        }
+        // 판매원이 관람객에게 티켓을 판매함
+        ticketSeller.sellTo(audience);
     }
 
 }
--- src/main/java/io/devfactory/objects/ticket/TicketSeller.java
+++ src/main/java/io/devfactory/objects/ticket/TicketSeller.java
@@ -9,8 +9,10 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        // 판매원 매표소의 티켓을 관람객에게 주고
+        // 관람객이 준 현금만큼 판매금액이 증가
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 
 }
