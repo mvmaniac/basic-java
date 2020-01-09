9829758a70070ad227e342db4636a0a5150a424e
yhs <mvmaniaz@gmail.com>
2020-01-10 00:34:51 +0900

    [수정] Objects: 1장 티켓 판매 애플리케이션 수정 2차 - Audience 클래스로부터 Bag 클래스를 캡슐화

--- src/main/java/io/devfactory/objects/ticket/Audience.java
+++ src/main/java/io/devfactory/objects/ticket/Audience.java
@@ -10,25 +10,7 @@ public class Audience {
     }
 
     public long buy(Ticket ticket) {
-        if (bag.hasInvitation()) {
-            // 관람객이 가방에 초대장이 있는 경우
-            // 관람객이 티켓을 가방에 넣음
-            bag.putTicket(ticket);
-
-            // 초대장이 있기 떄문에 가격 0
-            return 0L;
-
-        } else {
-            // 관람객이 가방에 초대장이 없는 경우
-            // 관람객이 티켓을 가방에 넣음
-            bag.putTicket(ticket);
-
-            // 관람객 가방에 티켓 가격만큼 현금이 차감
-            bag.minusAmount(ticket.getFee());
-
-            // 티켓 가격
-            return ticket.getFee();
-        }
+        return bag.hold(ticket);
     }
 
 }
--- src/main/java/io/devfactory/objects/ticket/Bag.java
+++ src/main/java/io/devfactory/objects/ticket/Bag.java
@@ -18,24 +18,46 @@ public class Bag {
         this.invitation = invitation;
         this.amount = amount;
     }
+    
+    public long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            // 관람객이 가방에 초대장이 있는 경우
+            // 관람객이 티켓을 가방에 넣음
+            putTicket(ticket);
 
-    public void putTicket(Ticket ticket) {
+            // 초대장이 있기 떄문에 가격 0
+            return 0L;
+
+        } else {
+            // 관람객이 가방에 초대장이 없는 경우
+            // 관람객이 티켓을 가방에 넣음
+            putTicket(ticket);
+
+            // 관람객 가방에 티켓 가격만큼 현금이 차감
+            minusAmount(ticket.getFee());
+
+            // 티켓 가격
+            return ticket.getFee();
+        }
+    }
+
+    private void putTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public boolean hasInvitation() {
+    private boolean hasInvitation() {
         return Objects.nonNull(invitation);
     }
 
-    public boolean hasTicket() {
+    private boolean hasTicket() {
         return Objects.nonNull(ticket);
     }
 
-    public void minusAmount(long amount) {
+    private void minusAmount(long amount) {
         this.amount -= amount;
     }
 
-    public void plusAmount(long amount) {
+    private void plusAmount(long amount) {
         this.amount += amount;
     }
 
