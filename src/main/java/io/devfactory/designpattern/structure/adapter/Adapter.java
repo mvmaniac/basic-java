a9952d0802f33f5e65ac94dbba41b5e1235e6638
yhs <mvmaniaz@gmail.com>
2020-12-21 23:08:49 +0900

    [추가] DesignPattern: 어댑터 패턴

---
+++ src/main/java/io/devfactory/designpattern/structure/adapter/Adapter.java
@@ -0,0 +1,10 @@
+package io.devfactory.designpattern.structure.adapter;
+
+// 어댑터 인터페이스
+public interface Adapter {
+
+  int twiceOf(int num);
+
+  int halfOf(int num);
+
+}
---
+++ src/main/java/io/devfactory/designpattern/structure/adapter/service/Math.java
@@ -0,0 +1,20 @@
+package io.devfactory.designpattern.structure.adapter.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Math {
+
+  // 입력한 값이 2배 증가
+  public double towTime(double num) {
+    log.debug("실수 2배 적용합니다.");
+    return num * 2;
+  }
+
+  // 입력 한 값이 절반으로 감소
+  public double halfTime(double num) {
+    log.debug("실수 1/2배 적용합니다.");
+    return num / 2;
+  }
+
+}
