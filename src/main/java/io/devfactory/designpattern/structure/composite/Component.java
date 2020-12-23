4e5e327a47f2054ddba4f37e3d474ed26bddde2c
yhs <mvmaniaz@gmail.com>
2020-12-23 23:26:37 +0900

    [추가] DesignPattern: 복합체 패턴

---
+++ src/main/java/io/devfactory/designpattern/structure/composite/Component.java
@@ -0,0 +1,13 @@
+package io.devfactory.designpattern.structure.composite;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// 추상화로 생성
+@Getter
+@Setter
+public abstract class Component {
+
+  private String name;
+
+}
