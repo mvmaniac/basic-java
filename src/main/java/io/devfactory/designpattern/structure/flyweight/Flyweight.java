2a08b8392e6afbe2cadf07b7f25545a0ab9f9796
yhs <mvmaniaz@gmail.com>
2020-12-28 23:36:40 +0900

    [추가] DesignPattern: 플라이웨이트 패턴

---
+++ src/main/java/io/devfactory/designpattern/structure/flyweight/Flyweight.java
@@ -0,0 +1,8 @@
+package io.devfactory.designpattern.structure.flyweight;
+
+// 공유 객체의 인터페이스
+public interface Flyweight {
+
+  String code();
+
+}
---
+++ src/main/java/io/devfactory/designpattern/structure/flyweight/service/MorseG.java
@@ -0,0 +1,20 @@
+package io.devfactory.designpattern.structure.flyweight.service;
+
+import io.devfactory.designpattern.structure.flyweight.Flyweight;
+import lombok.extern.slf4j.Slf4j;
+
+// 본질적 상태의 공유객체, 본질적 공유 상태를 유지하기 위해 변수가 존재하지 않음
+// 따라서 객체의 상탯값 변경에 따라 영향을 받는 사이드 이펙트가 발생하지 않음
+@Slf4j
+public class MorseG implements Flyweight {
+
+  public MorseG() {
+    log.debug("MorseG 을(를) 생성하였습니다.");
+  }
+
+  @Override
+  public String code() {
+    return "**-*";
+  }
+
+}
